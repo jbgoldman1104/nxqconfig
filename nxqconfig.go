e243f4ae441c0eb6727453cfb3c87a242c71d6b6
Golden Winner <131433245+jbgoldman1104@users.noreply.github.com>
2024-03-12 06:22:28 +0900

    Update nxqconfig.go

--- nxqconfig.go
+++ nxqconfig.go
@@ -3,5 +3,7 @@ package nxqconfig
 // NexQloud Config
 const (
 	MaintenanceWallet = "nxq1c9sumtfpgkvwfs6mxm09p5a92ps2jej8gmu73t"
-	contractAddress = "0xA086712B80472B6d03e957516FD3e1ba7EcE10cd"
+	ContractAddress = "0xA086712B80472B6d03e957516FD3e1ba7EcE10cd"
+	MinValidatorToken = 50
+	MinValidatorNFTBalance = 5
 )

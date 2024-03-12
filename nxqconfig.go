244ed09313762d3fa7f90db811bc3220e4f4eb16
jbgoldman1104 <jbgoldman1104@gmail.com>
2024-03-12 13:15:51 +0000

    change nft contract address

--- nxqconfig.go
+++ nxqconfig.go
@@ -3,7 +3,7 @@ package nxqconfig
 // NexQloud Config
 const (
 	MaintenanceWallet      = "nxq1c9sumtfpgkvwfs6mxm09p5a92ps2jej8gmu73t"
-	ContractAddress        = "0xA086712B80472B6d03e957516FD3e1ba7EcE10cd"
+	ContractAddress        = "0xCdd1a022500bCC34302Da9d8e082E098424de35a"
 	MinValidatorToken      = 50
 	MinValidatorNFTBalance = 5
 )
